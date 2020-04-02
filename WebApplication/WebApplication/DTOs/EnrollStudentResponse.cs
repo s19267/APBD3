bc7c1209934698f3842ddc514357a2c808529caa
Maciej <petrykowski99@gmail.com>
2020-04-02 20:49:25 +0200

    cw5

---
+++ WebApplication/WebApplication/DTOs/EnrollStudentResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.DTOs
+{
+    public class EnrollStudentResponse
+    {
+        [Required]
+        public string IndexNumber { get; set; }
+        [Required]
+        public String FirstName { get; set; }
+        [Required]
+        public String LastName { get; set; }
+        [Required]
+        public String BirthDate { get; set; }
+        [Required]
+        public string Studies { get; set; }
+    }
+}
---
+++ WebApplication/WebApplication/DTOs/EnrollmentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication.DTOs
+{
+    public class EnrollmentResponse
+    {
+        public int IdEnrollment { get; set; }
+        public int Semester { get; set; }
+        public int  IdStudy { get; set; }
+        public String  StartDate { get; set; }
+    }
+}
