1cfc4457ff5c4bfb392e1ac250aaa69cb955c165
Maciej <petrykowski99@gmail.com>
2020-04-04 19:51:19 +0200

    cw5-1

--- WebApplication/WebApplication/Controllers/EnrollmentsController.cs
+++ WebApplication/WebApplication/Controllers/EnrollmentsController.cs
@@ -14,7 +14,8 @@ namespace WebApplication.Controllers
         [HttpPost]
         public IActionResult CreateStudent(EnrollStudentResponse request)
         {
-            using (SqlConnection con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s19267;Integrated Security=True"))
+            using (SqlConnection con =
+                new SqlConnection("Data Source=db-mssql;Initial Catalog=s19267;Integrated Security=True"))
             using (SqlCommand com = new SqlCommand())
             {
                 com.Connection = con;
@@ -22,7 +23,7 @@ namespace WebApplication.Controllers
                 con.Open();
                 var tran = con.BeginTransaction();
                 com.Transaction = tran;
-                
+
                 //idStudies
                 com.CommandText = "select IdStudy from Studies where name=@StudyName";
                 com.Parameters.AddWithValue("StudyName", request.Studies);
@@ -32,9 +33,10 @@ namespace WebApplication.Controllers
                     tran.Rollback();
                     return BadRequest("Studia nie istnieja");
                 }
+
                 int idStudies = (int) dr["IdStudy"];
                 dr.Close();
-                
+
                 // Enrollment
                 com.CommandText =
                     "select * from Enrollment where Semester=1 and IdStudy=@idStudies and StartDate=(select max(StartDate) from Enrollment)";
@@ -52,36 +54,83 @@ namespace WebApplication.Controllers
                     dr = com.ExecuteReader();
                     dr.Read();
                 }
+
                 int idEnrollment = (int) dr["IdEnrollment"];
                 int semesterEnrollment = (int) dr["Semester"];
                 int idStudyEnrollment = (int) dr["IdStudy"];
                 string startDateEnrollment = dr["StartDate"].ToString();
                 dr.Close();
-                
+
                 //idStudent unique
                 com.CommandText = "select IndexNumber from Student where IndexNumber=@IndexStudent";
                 com.Parameters.AddWithValue("IndexStudent", request.IndexNumber);
-                dr=com.ExecuteReader();
+                dr = com.ExecuteReader();
                 if (dr.Read())
                 {
                     tran.Rollback();
                     return BadRequest("student z tym indexem juz istnieje");
                 }
+
                 dr.Close();
-                
+
                 com.CommandText =
                     "insert into Student(IndexNumber,FirstName,LastName,BirthDate,IdEnrollment) values (@IndexStudent,@FirstName,@LastName,Convert(varchar(40),@BirthDate,4) ,@idStudies)";
                 com.Parameters.AddWithValue("FirstName", request.FirstName);
                 com.Parameters.AddWithValue("LastName", request.LastName);
+                request.BirthDate = request.BirthDate.Substring(6) + "-" + request.BirthDate.Substring(3, 2) + "-" +
+                                    request.BirthDate.Substring(0, 2);
                 com.Parameters.AddWithValue("BirthDate", request.BirthDate);
                 com.ExecuteNonQuery();
-                
+
                 tran.Commit();
-                return CreatedAtAction("createStudent", new EnrollmentResponse{IdEnrollment = idEnrollment,IdStudy = idStudyEnrollment,Semester =semesterEnrollment,StartDate = startDateEnrollment});
+                return CreatedAtAction("createStudent",
+                    new EnrollmentResponse
+                    {
+                        IdEnrollment = idEnrollment, IdStudy = idStudyEnrollment, Semester = semesterEnrollment,
+                        StartDate = startDateEnrollment
+                    });
+            }
+        }
+
+        [HttpPost("promotions")]
+        public IActionResult Promote(EnrollPromoteRequest enrollPromoteRequest)
+        {
+            using (SqlConnection con =
+                new SqlConnection("Data Source=db-mssql;Initial Catalog=s19267;Integrated Security=True"))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+
+                con.Open();
+                var tran = con.BeginTransaction();
+                com.Transaction = tran;
+                
+                //idStudy
+                com.CommandText = "select IdStudy from Studies where name=@studiesName";
+                com.Parameters.AddWithValue("studiesName", enrollPromoteRequest.studies);
+                var dr = com.ExecuteReader();
+                if (!dr.Read())
+                {
+                    tran.Rollback();
+                    return BadRequest("Studia nie istnieja");
+                }
+                int idStudies = (int) dr["IdStudy"];
+                dr.Close();
+                
+                //Enrollments
+                com.CommandText = "select IdEnrollment from Enrollment where Semester=@semester and IdStudy=@idStudy";
+                com.Parameters.AddWithValue("semester", enrollPromoteRequest.semster);
+                com.Parameters.AddWithValue("idStudy", idStudies);
+                dr = com.ExecuteReader();
+                if (!dr.Read())
+                {
+                    tran.Rollback();
+                    return BadRequest("Studia nie istnieja");
+                }
+                
                 
             }
-
-            
+            return 
         }
     }
 }
---
+++ WebApplication/WebApplication/DTOs/EnrollPromoteRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication.DTOs
+{
+    public class EnrollPromoteRequest
+    {
+        public string studies { get; set; }
+        public int semster { get; set; }
+    }
+}
